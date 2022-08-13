dea47973f654b26a7b6de7073f3a47a6ef267c4c
endtailer <surajofficial2000@gmail.com>
2022-08-13 11:36:41 +0530

    Initial commit

---
+++ app/src/main/java/com/example/welcomeactivity/welcomeactivity.kt
@@ -0,0 +1,11 @@
+package com.example.welcomeactivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class welcomeactivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.welcomeactivity)
+    }
+}
