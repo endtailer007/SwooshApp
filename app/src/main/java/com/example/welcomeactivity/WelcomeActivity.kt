05d26e80991a0dcc11f9e79d6fdefe75bc5fc7e0
endtailer <surajofficial2000@gmail.com>
2022-08-13 14:13:07 +0530

    commit 2

---
+++ app/src/main/java/com/example/welcomeactivity/WelcomeActivity.kt
@@ -0,0 +1,33 @@
+package com.example.welcomeactivity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.welcome_activity)
+        //val button= findViewById<Button>(R.id.getstartedbutton)
+       findViewById<Button>(R.id.getstartedbutton).setOnClickListener{
+            val leagueIntent=Intent(this,LeagueActivity::class.java)
+           startActivity(leagueIntent)
+       }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
