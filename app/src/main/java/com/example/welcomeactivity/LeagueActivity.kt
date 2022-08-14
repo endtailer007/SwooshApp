a14aa8f008e0a65e97aaa91e97371a47bbf3d14b
endtailer <surajofficial2000@gmail.com>
2022-08-14 13:13:47 +0530

    commit 3

--- app/src/main/java/com/example/welcomeactivity/LeagueActivity.kt
+++ app/src/main/java/com/example/welcomeactivity/LeagueActivity.kt
@@ -3,7 +3,7 @@ package com.example.welcomeactivity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class LeagueActivity : AppCompatActivity() {
+class LeagueActivity : Baseactivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
--- app/src/main/java/com/example/welcomeactivity/WelcomeActivity.kt
+++ app/src/main/java/com/example/welcomeactivity/WelcomeActivity.kt
@@ -5,9 +5,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.core.content.ContextCompat.startActivity
 
 
-class WelcomeActivity : AppCompatActivity() {
+class WelcomeActivity : Baseactivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.welcome_activity)
